97ea126e7b4dcb9566b971e88cb3655f02a3cf44
Asncodes-80 <asn80.asn@hotmail.com>
2024-09-22 15:44:50 +0330

    fix: edit poetry command

--- psim.sh
+++ psim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    poretry run python /home/$USER/Development/modbus-rtu-data-collector/main.py
+    poetry run python /home/$USER/Development/modbus-rtu-data-collector/main.py
     sleep 3
 done
